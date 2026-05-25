2ad28db04dbd51d179aa5158167fc0485ca871dc
4nn2662 <4nn2662@gmail.com>
2026-05-25 21:47:49 +0200

    Update first_queries.sql

--- first_queries.sql
+++ first_queries.sql
@@ -8,13 +8,26 @@ SELECT COUNT(*) FROM customer
 WHERE first_name LIKE 'D%';
 
 
--- Total amount spent by customer
+-- Which customers spent more than a 100?
 SELECT customer_id, SUM(amount) AS total_spent FROM payment
 GROUP BY customer_id
-ORDER BY total_Spent DESC:
+HAVING SUM(amount) > 100
+ORDER BY total_Spent DESC;
+
 
 -- What customer  has the lowest cusotmer ID number whose name ends with an 'A' and has an address ID lower than 40?
 SELECT first_name, last_name FROM customer
 WHERE first_name LIKE '%A'
 AND address_id < 40
 ORDER BY customer_id;
+
+
+--Which move rating are the most common?
+SELECT rating, COUNT(*) AS total_movies FROM film
+GROUPBY rating
+ORDER BY total_movies DESC;
+
+--What is the average payment amount per customer?
+SELECT customer_id, AVG(amount) AS avg_payment FROM payment
+GROUP BY payment
+ORDER BY avg_payment DESC;
