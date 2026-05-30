74a6141c456d531acd0ae73394543db6ac7450e1
4nn2662 <4nn2662@gmail.com>
2026-05-30 16:01:01 +0200

    Update joins_practice.sql

--- joins_practice.sql
+++ joins_practice.sql
@@ -3,7 +3,7 @@
 SELECT customer.first_name, customer.last_name, COUNT(*) AS total_rents FROM customer
 INNER JOIN rental
 ON customer.customer_id = rental.customer_id
-GROUP BY customer.customer_id 
+GROUP BY customer.customer_id, customer.first_name, customer.last_name
 ORDER BY total_rents DESC;
 
 2. Which customers spent the most money?
@@ -17,11 +17,10 @@ ORDER BY total_payments DESC;
 
 3. Which customers never rented a movie?
 
-SELECT customer.first_name, customer.last_name
-FROM customer
+SELECT customer.first_name, customer.last_name FROM customer
 LEFT JOIN rental
 ON customer.customer_id = rental.customer_id
-WHERE rental.customer_id IS NULL;
+WHERE rental.rental_id IS NULL;
 
 4. Which movies have never been rented?
 
@@ -32,10 +31,9 @@ LEFT JOIN rental
 ON inventory.inventory_id = rental.inventory_id
 WHERE rental.rental_id IS NULL;
 
-5. Which customer neves never made a payment?
+5. Which customers never never made a payment?
 
-SELECT customer.customer_id
-FROM customer
+SELECT customer.customer_id, customer.first_name, customer.last_name FROM customer
 LEFT JOIN payment
 ON customer.customer_id = payment.customer_id
 WHERE payment.payment_id IS NULL;
