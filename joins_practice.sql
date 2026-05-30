999ef7cdbd1643cdadb153a8f7646f6fc0bc3578
4nn2662 <4nn2662@gmail.com>
2026-05-30 15:53:33 +0200

    Create joins_practice.sql
    Again, examples are on DVD Rental Base

---
+++ joins_practice.sql
@@ -0,0 +1,41 @@
+1. Which customers rented the most movies?
+
+SELECT customer.first_name, customer.last_name, COUNT(*) AS total_rents FROM customer
+INNER JOIN rental
+ON customer.customer_id = rental.customer_id
+GROUP BY customer.customer_id 
+ORDER BY total_rents DESC;
+
+2. Which customers spent the most money?
+
+SELECT customer.first_name, customer.last_name, SUM(amount) AS total_payments FROM customer
+INNER JOIN payment
+ON customer.customer_id = payment.customer_id
+GROUP BY customer.customer_id, customer.first_name, customer.last_name 
+ORDER BY total_payments DESC;
+
+
+3. Which customers never rented a movie?
+
+SELECT customer.first_name, customer.last_name
+FROM customer
+LEFT JOIN rental
+ON customer.customer_id = rental.customer_id
+WHERE rental.customer_id IS NULL;
+
+4. Which movies have never been rented?
+
+SELECT DISTINCT film.title FROM film
+LEFT JOIN inventory
+ON film.film_id = inventory.film_id
+LEFT JOIN rental
+ON inventory.inventory_id = rental.inventory_id
+WHERE rental.rental_id IS NULL;
+
+5. Which customer neves never made a payment?
+
+SELECT customer.customer_id
+FROM customer
+LEFT JOIN payment
+ON customer.customer_id = payment.customer_id
+WHERE payment.payment_id IS NULL;
